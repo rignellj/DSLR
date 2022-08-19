de6498dfdfa39b4012c5044c6b1ea4efd948f0c4
Jere Rignell <jere.rignell@gmail.com>
2022-08-19 16:20:56 +0300

    data_analysis: add Dslr class

---
+++ classes/__init__.py
@@ -0,0 +1,1 @@
+from .dslr import Dslr
---
+++ classes/dslr.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+class Dslr():
+
+    def __init__(self, data_frame: pd.DataFrame, columns: pd.Index) -> None:
+        self._columns = columns
+        self._df = data_frame
+        self._summary = {}
+
+    def count_rows(self):
+        for column in self._columns:
+            count = 0
+            sum = 0
+            for value in self._df[column]:
+                if pd.notna(value):
+                    count += 1
+                    sum += value
+            self._summary.update({column: {'Count': count, 'Sum': sum}})
+
+    def count_mean(self):
+        for column in self._columns:
+            data_column = self._summary[column]
+            count = data_column['Count']
+            sum = data_column['Sum']
+            mean = count / sum
+            data_column.update({**data_column, 'Mean': mean})
