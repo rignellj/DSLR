12deb8ab2d75eefe47be5896bb2df0e8ba030cf6
Jere Rignell <jere.rignell@gmail.com>
2022-09-02 17:20:05 +0300

    visualization: add houses, subjects and pvalue constants

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,7 @@
 NUMBER_TYPES = ['int64', 'float64']
+HOUSES = ['Ravenclaw', 'Slytherin', 'Gryffindor', 'Hufflepuff']
+SUBJECTS = ['Ancient Runes', 'Arithmancy', 'Astronomy', 'Care of Magical Creatures',
+       'Charms', 'Defense Against the Dark Arts', 'Divination', 'Flying',
+       'Herbology', 'History of Magic', 'Hogwarts House', 'Index',
+       'Muggle Studies', 'Potions', 'Transfiguration']
+P_VALUE = 0.05
